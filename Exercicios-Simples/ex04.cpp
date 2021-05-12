6f6ec8d546165e2a116bc5bf8eb27f992c8695ad
Raphael C. Santos <raphaelscarmo@gmail.com>
2021-05-12 01:33:37 -0300

    Create ex04.cpp

---
+++ Exercicios-Simples/ex04.cpp
@@ -0,0 +1,31 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+/* Exercicio simples - tirar a média.
+    Ler as notas dos alunos, total = nota * peso. E a média vai ser Media = total /11.
+    Usei "fixed" pra limitar o número de casas após o ponto, quando o número é ponto flutuante
+    O "cout.precision(5)" vai imprimir apenas 5 casas após a virgula.
+    declarei todas as variaveis como ponto flutante, ou seja float.
+*/
+
+int main() {
+	float nota1,nota2,Media,peso1,peso2,total1,total2,total3;
+	
+	cin>>nota1;
+	cin>>nota2;
+	
+	peso1 = 3.5;
+	peso2 = 7.5;
+	
+	total1 = nota1 * peso1;
+	total2 = nota2 * peso2;
+	total3 = total1 + total2;
+	Media = total3 / 11; 
+	
+	cout << fixed << showpoint;
+    cout << setprecision(5);
+	cout << "MEDIA = " << Media << endl; 
+	
+	return 0;
+}
